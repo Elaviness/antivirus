2dfe03189f985411378309757cf9da20f167a3f3
Ведерников Николай <vst515@yandex.ru>
2020-05-14 10:36:35 +0300

    Add sort in file

--- SignatureBase/SignatureBase/Work_with_file.cs
+++ SignatureBase/SignatureBase/Work_with_file.cs
@@ -1,39 +1,51 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace SignatureBase
 {
 	public class Work_with_data
 	{
-		public string SIGNATURE_DB_FILE_NAME = "SignatureDB.txt",
-			PATH_TO_DB_FILE;
+	
+		public string SIGNATURE_DB_FILE_NAME = "SignatureDB.txt", PATH_TO_DB_FILE;
+
+
 
 		public Work_with_data()
 		{
 			PATH_TO_DB_FILE = Path.Combine(Directory.GetCurrentDirectory(), SIGNATURE_DB_FILE_NAME);
 		}
 
-		public void Write_to_file(string virus_name, int signature_length, int signature_prefix, string signature_hash, int offset_bigin, int offset_end)
+		public void Write_to_end_file(string virus_name, int signature_length, int signature_prefix, string signature_hash, int offset_bigin, int offset_end)
         {
-			string text = virus_name + signature_length.ToString() + signature_prefix.ToString() + signature_hash + offset_bigin.ToString() + offset_end.ToString();
-			byte[] strBytes = System.Text.Encoding.Unicode.GetBytes(text);
-			
-			BinaryWriter writer = new BinaryWriter(File.Open(PATH_TO_DB_FILE, FileMode.OpenOrCreate));
-            writer.Write(strBytes);
-            
-        }
+
+			string text = virus_name + " " + signature_length.ToString()+ " " + signature_prefix.ToString() + " " +
+				signature_hash + " " + offset_bigin.ToString() + " " + offset_end.ToString();
+			using (StreamWriter writer = new StreamWriter(PATH_TO_DB_FILE, true, System.Text.Encoding.Default))
+			{
+				writer.WriteLine(text);
+			}
+		}
+
 
 		public string Read_from_file_one_line(int number_line)
         {
 			try
-			{
-				return (File.ReadLines(SIGNATURE_DB_FILE_NAME).Skip(number_line).Take(1)).ToString();
-
-			} catch (FileNotFoundException)
+			{ 
+				return File.ReadLines(PATH_TO_DB_FILE).ElementAt(number_line); ;
+			} 
+			catch (Exception)
             {
 				return "-1";
             }
         }
+
+
+		public void Write_in_file_in_line()
+		{
+			
+		}
 	}
 }
