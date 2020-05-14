77a9d2b8e64a3ac389db4c7c7cf89710a4fc72c5
Ведерников Николай <vst515@yandex.ru>
2020-05-14 11:56:26 +0300

    Add signature structure

--- SignatureBase/SignatureBase/SignatureIO.cs
+++ SignatureBase/SignatureBase/SignatureIO.cs
@@ -4,6 +4,11 @@ namespace SignatureBase
 {
     public class Signature
     {
+        string name;
+        int signature_length; // k символов сигнатуры (n<=k)
+        int signature_prefix; // n символов сигнатуры, которые хранятся в базе для поиска вхождения
+        string signature_hash; // SHA256-хэш сигнатуры
+        int offset_begin; //минимальное смещение первого символа сигнатуры относительно начала
+        int offset_end; //максимальное смещение оь начала файла
     }
-
 }
