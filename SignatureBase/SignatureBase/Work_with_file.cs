c2a1dcac65e127bbac74cbbd65deb514f54ccc03
Ведерников Николай <vst515@yandex.ru>
2020-05-15 12:31:55 +0300

    Add binary search prefix

--- SignatureBase/SignatureBase/Work_with_file.cs
+++ SignatureBase/SignatureBase/Work_with_file.cs
@@ -11,41 +11,38 @@ namespace SignatureBase
 		public Mutex Mutex_WWD = new Mutex(false, "Mutex_WWD");
 		public string SIGNATURE_DB_FILE_NAME = "SignatureDB.txt", PATH_TO_DB_FILE;
 
-
-
 		public Work_with_data()
 		{
 			PATH_TO_DB_FILE = Path.Combine(Directory.GetCurrentDirectory(), SIGNATURE_DB_FILE_NAME);
 		}
 
 		public void Write_to_end_file(string virus_name, int signature_length, int signature_prefix, string signature_hash, int offset_bigin, int offset_end)
-        {
-
-			string text = virus_name + " " + signature_length.ToString()+ " " + signature_prefix.ToString() + " " +
+		{
+			string text = virus_name + " " + signature_length.ToString() + " " + signature_prefix.ToString() + " " +
 				signature_hash + " " + offset_bigin.ToString() + " " + offset_end.ToString();
+			Mutex_WWD.WaitOne();
 			using (StreamWriter writer = new StreamWriter(PATH_TO_DB_FILE, true, System.Text.Encoding.Default))
 			{
 				writer.WriteLine(text);
 			}
+			Mutex_WWD.ReleaseMutex();
 		}
 
 
 		public string Read_from_file_one_line(int number_line)
-        {
-			try
-			{ 
-				return File.ReadLines(PATH_TO_DB_FILE).ElementAt(number_line); ;
-			} 
-			catch (Exception)
-            {
-				return "-1";
-            }
-        }
-
-
-		public void Write_in_file_in_line()
 		{
-			
+			try
+			{
+				Mutex_WWD.WaitOne();
+				string line = File.ReadLines(PATH_TO_DB_FILE).ElementAt(number_line);
+				Mutex_WWD.ReleaseMutex();
+				return line;
+			}
+			catch (Exception)
+			{
+				return "-1";
+			}
 		}
+
 	}
 }
--- Test_in_console/Program.cs
+++ Test_in_console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SignatureBase
 {
@@ -11,15 +7,15 @@ namespace SignatureBase
         static void Main(string[] args)
         {
             Signature my_sign = new Signature();
-            my_sign.LineSplit("virus 5 hello jshgdfjsygdvhd 1 10");
-            string fst = "zxc";
-            string snd = "abc";
+            my_sign.LineSplit("Petya 6 hello jshgdfjsygdvhd 1 10");
 
-            Console.WriteLine(fst.CompareTo(snd));
-            foreach (var itm in my_sign.Signature_list)
-            {
-                Console.WriteLine(itm.name);
-            }
+            my_sign.LineSplit("Vasya 6 world jshgdfjsygdvhd 1 10");
+
+            my_sign.LineSplit("Alesha 6 aloha jshgdfjsygdvhd 1 10");
+
+            my_sign.Signature_list.Sort();
+            Console.WriteLine(my_sign.Find_prefix("world"));
+
             Console.ReadKey();
         }
     }
