35cf091e3387ef96d0c79ef052f6af330ba8fbc9
Ведерников Николай <vst515@yandex.ru>
2020-05-15 16:54:26 +0300

    add gitignore and test in console

--- Test_in_console/Program.cs
+++ Test_in_console/Program.cs
@@ -4,17 +4,34 @@ namespace SignatureBase
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Signature my_sign = new Signature();
+            Console.WriteLine("До сортировки по префиксу");
+
             my_sign.LineSplit("Petya 6 hello jshgdfjsygdvhd 1 10");
 
-            my_sign.LineSplit("Vasya 6 world jshgdfjsygdvhd 1 10");
+            my_sign.LineSplit("Alesha 6 world jshgdfjsygdvhd 1 10");
 
-            my_sign.LineSplit("Alesha 6 aloha jshgdfjsygdvhd 1 10");
+            my_sign.LineSplit("Zinaida 6 aloha jshgdfjsygdvhd 1 10");
+
+            foreach (Signature i in my_sign.Signature_list)
+            {
+                Console.WriteLine("Имя вируса: {0}\t Префикс: {1}", i.name, i.signature_prefix);
+            }
+            Console.WriteLine();
+            Console.WriteLine("После сортировки по префиксу");
 
             my_sign.Signature_list.Sort();
-            Console.WriteLine(my_sign.Find_prefix("world"));
+
+            foreach (Signature i in my_sign.Signature_list)
+            {
+                Console.WriteLine("Имя вируса: {0}\t Префикс: {1}", i.name, i.signature_prefix);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Индекс записи с префиксом \"word\": {0}", my_sign.Find_prefix("world"));
+          
 
             Console.ReadKey();
         }
