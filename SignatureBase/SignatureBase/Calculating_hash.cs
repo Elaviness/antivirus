80c4f6b31a55c7ee37872cb14275447d16faceec
Ведерников Николай <vst515@yandex.ru>
2020-06-06 14:39:09 +0300

    Add Calculating_hash class

---
+++ SignatureBase/SignatureBase/Calculating_hash.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace SignatureBase
+{
+    class Calculating_hash
+    {
+        public bool Check_hash(string line, string hash_record)
+        {
+            var msgBytes = Encoding.ASCII.GetBytes(line);
+
+            var sha = new System.Security.Cryptography.SHA256Managed();
+            var hash = sha.ComputeHash(msgBytes);
+
+            string hash_in_hex = "";
+            foreach (byte b in hash)
+            {
+                hash_in_hex += b.ToString("x2");
+            }
+            if (string.Compare(hash_in_hex, hash_record) == 0)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
